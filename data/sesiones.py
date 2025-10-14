6d995f34567463a308b512d00df4a5c2d6a28315
Matías Aarón Paná <matiasaaron.pana@cba.gov.ar>
2025-10-13 21:31:18 -0300

    feat: anterior estructura de proyecto eliminada

--- data/sesiones.py
+++
@@ -1,5 +0,0 @@
-from entities.Sesion import Sesion
-from datetime import datetime
-from data.usuarios import usuario_data
-
-Sesion0 = Sesion(fechaHoraDesde=datetime.now(), fechaHoraHasta=None, usuario=usuario_data[2])
--- entities/Reparacion.py
+++
@@ -1,2 +0,0 @@
-class Reparacion:
-    pass
