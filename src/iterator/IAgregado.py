cf7466ae50df3f6cb1c8abacea82f5dcf117194b
Florencia Sofia Terreno Monla <terrenoflorencia13@gmail.com>
2025-11-05 15:30:06 -0300

    fix: modificacion de la interfaz iagregado para que unicamente quede la firma

--- src/iterator/IAgregado.py
+++ src/iterator/IAgregado.py
@@ -1,29 +1,7 @@
-class IAgregado:
-    @staticmethod
+from abc import ABC, abstractmethod
+
+class IAgregado(ABC):
+
+    @abstractmethod
     def crearIterador(self, listaElementos: list) -> object:
-        if not listaElementos:
-            return None
-
-        # Importaciones aquí para romper el ciclo
-        from entities.EventoSismico import EventoSismico
-        from entities.SerieTemporal import SerieTemporal
-        from entities.MuestraSismica import MuestraSismica
-        from entities.DetalleMuestraSismica import DetalleMuestraSismica
-
-        from iterator.IIteradorEventosSismicos import IteradorEventosSismicos
-        from iterator.IIteradorSeriesTemporales import IteradorSeriesTemporales
-        from iterator.IIteradorMuestrasSismicas import IteradorMuestrasSismicas
-        from iterator.IIteradorDetallesMuestraSismica import IteradorDetallesMuestraSismica
-
-        primerElemento = listaElementos[0]
-
-        if isinstance(primerElemento, EventoSismico):
-            return IteradorEventosSismicos(listaElementos)
-        elif isinstance(primerElemento, SerieTemporal):
-            return IteradorSeriesTemporales(listaElementos)
-        elif isinstance(primerElemento, MuestraSismica):
-            return IteradorMuestrasSismicas(listaElementos)
-        elif isinstance(primerElemento, DetalleMuestraSismica):
-            return IteradorDetallesMuestraSismica(listaElementos)
-        else:
-            raise TypeError("Tipo de lista no soportado para crear iterador.")
+        pass
